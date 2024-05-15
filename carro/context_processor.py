a8cdbcb61422187286b5f0ff1e06dc24289b5eb4
parramiguel <ingparrasojo@gmail.com>
2024-05-15 12:58:51 -0500

    Primer Commit

--- carro/context_processor.py
+++ carro/context_processor.py
@@ -1,7 +1,11 @@
 def importe_total_carro(request):
     total=0
-    #if request.user.is_authenticated:
-    for key, value in request.session["carro"].items():
-        total=total+float(value["precio"])
+    if request.user.is_authenticated:
+        for key, value in request.session["carro"].items():
+            total=total+float(value["precio"])
+    
+    else:
+        total="Debes estar logeado"
+
         
     return {"importe_total_carro":total}
